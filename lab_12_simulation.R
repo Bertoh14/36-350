2a1beb500356c220d8e9c2c3bd1eb8553ff15e42
Bertoh14 <robbyoh@gmail.com>
2018-04-19 18:01:18 -0400

    model_select

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -4,3 +4,18 @@ generate_data = function(n,p){
   return(list(covariates= mat, responses=vec))
 }
 
+model_select = function(covariates, responses, cutoff){
+  n = length(responses)
+  vec = vector(length=n)
+  lin = summary(lm(responses ~ covariates))
+  for (i in 1:n) {
+    vec[i] = (lin[[i]]$coefficients)[,4][2]
+  }
+  new = summary(lm(responses ~ covariates[,vec<=cutoff]))
+  if(sum(vec<= cutoff) == 0) return(c())
+  vec2 = vector(sum(vec <= cutoff))
+  for (i in 1:sum(vec <= cutoff)) {
+    vec2[i] = (new[[i]]$coefficients)[,4][2]
+  }
+  return(vec2)
+}
