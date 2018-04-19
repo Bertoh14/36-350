243c330043959b3769e59cf19c44ea9bb8e1dca9
Bertoh14 <robbyoh@gmail.com>
2018-04-19 18:08:21 -0400

    run_simulation

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -15,7 +15,17 @@ model_select = function(covariates, responses, cutoff){
   if(sum(vec<= cutoff) == 0) return(c())
   vec2 = vector(sum(vec <= cutoff))
   for (i in 1:sum(vec <= cutoff)) {
-    vec2[i] = (new[[i]]$coefficients)[,4][2]
+    vec2[i] = (new[[i]]$coefficients )[,4][2]
   }
   return(vec2)
+}
+
+run_simulation = function(n_trials,n,p,cutoff){
+  ret = c()
+  for (i in 1:n_trials) {
+    gen = generate_data(n,p)
+    mod = model_select(gen$covariates, gen$responses, cutoff)
+    ret = c(ret,mod)
+  }
+  hist(ret)
 }
